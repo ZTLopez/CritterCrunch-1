de05b862aa4a6d428a28ab7df8008e1b16da7bba
ZTLopez <43570215+ZTLopez@users.noreply.github.com>
2019-01-20 02:44:53 +0000

    Add files via upload
    added walking animations, sprite and movement to grid

--- CritterCrunch/js/Game.js
+++ CritterCrunch/js/Game.js
@@ -2,8 +2,8 @@
 // config
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 720,
+  height: 720,
   physics: {
     default: 'arcade',
     arcade: {
@@ -23,12 +23,12 @@ var game = new Phaser.Game(config);
 var player, enemy = [], maxEnemies= 5;
 
 function preload() {
-    this.load.spritesheet("Critter", "assets/critter/critterSpriteSheet.png", { frameWidth: 49, frameHeight: 66 });
-    this.load.spritesheet("Fox","assets/critter/foxSpriteSheet.png", { frameWidth: 49, frameHeight: 66 });
+    this.load.spritesheet("Critter", "assets/critter/critterSpriteSheet.png", { frameWidth: 48, frameHeight: 64 });
+    this.load.spritesheet("Fox","assets/critter/foxSpriteSheet.png", { frameWidth: 48, frameHeight: 64 });
     this.load.spritesheet("heartAttack","assets/critter/heartsheet.png", {frameWidth:38, frameHeight: 30});
-    this.load.image("shadow","assets/critter/shadow.png");
+    //this.load.image("shadow","assets/critter/shadow.png");
     this.load.image("house", "assets/critter/house.png");
-    this.load.image("landscape", "assets/critter/tileseet.png");
+    this.load.image("landscape", "assets/critter/tilesheet.png");
     this.load.tilemapTiledJSON("level1", "assets/critter/level1.json");
 }
 
@@ -38,26 +38,64 @@ function create(){
 
     this.map = this.make.tilemap({ key: "level1" });
     var landscape = this.map.addTilesetImage("world", "landscape");
-    var home = this.map.addTileSetImage("house","house");
-    this.map.createStaticLayer("foreground2", landscape, 0, 0);
-    this.map.createStaticLayer("foreground", landscape, 0, 0);
+    var home = this.map.addTilesetImage("house","house");
+
     this.map.createStaticLayer("background", landscape, 0, 0);
+    this.map.createStaticLayer("foreground", [landscape, home], 0, 0);
+    this.map.createStaticLayer("foreground2", landscape, 0, 0);
+
     var destructLayer = this.map.createDynamicLayer("walls", landscape, 0, 0);
     destructLayer.setCollisionByProperty({collides: true});
 
-    var playerSpawn = map.findObject("playerSpawn", function (object) {
+    var playerSpawn = this.map.findObject("playerSpawn", function (object) {
         if (object.name === "playerSpawn") {
             return object;
         }
     });
-    
-    createPlayer.call(this.playerSpawn);
+
+    //createPlayer.call(this.playerSpawn);
+
+    player = new Critter (this, playerSpawn);
 
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-    
+
+    this.cameras.main.startFollow(player.critter, true, 0.5, 0.5);
+    this.cameras.main.zoom = 3.5;
+
+    createPlayerAnimations.call(this);
+
+
 }
 
 function update(){
+    player.update();
 
 }
+
+function createPlayerAnimations() {
+    this.anims.create({
+        key: 'walkDown',
+        frames: this.anims.generateFrameNumbers('Critter', {start: 7, end: 11}),
+        frameRate: 10,
+        repeat: 0
+    });
+    this.anims.create({
+        key: 'walkUp',
+        frames: this.anims.generateFrameNumbers('Critter', {start: 13, end: 17}),
+        frameRate: 10,
+        repeat: 0
+    });
+    this.anims.create({
+        key: 'walkSide',
+        frames: this.anims.generateFrameNumbers('Critter', {start: 18, end: 22}),
+        frameRate: 10,
+        repeat: 0
+    });
+    this.anims.create({
+        key: 'sideIdle',
+        frames: this.anims.generateFrameNumbers('Critter', {start: 2, end: 3}),
+        frameRate: 2,
+        repeat: -1
+    });
+  }
--- CritterCrunch/js/critterClasses.js
+++ CritterCrunch/js/critterClasses.js
@@ -2,25 +2,39 @@
 class Animals {
     constructor(scene,){
         this.scene = scene;
-        this.shadow = this.scene.add.sprite();
-        this.shadow.setDepth(1);
+        //this.shadow = this.scene.add.sprite();
+        //this.shadow.setDepth(1);
     }
     update(){
 
-    }   
-    
+    }
+
 }
 
 class Fox extends Animals {
-    constructor(){
-        super();
+    constructor(scene){
+        super(scene);
+        this.enemy = this.scene.physics.add.sprite(playerSpawn.x, playerSpawn.y, 'Fox', 4);
+        this.enemy.setCollideWorldBounds(true);
+
+        createEnemyAnimations.call(this);
     }
 }
 
 class Critter extends Animals {
-    constructor(){
-        super();
-        
+    constructor(scene, playerSpawn){
+        super(scene);
+
+        this.critter = this.scene.physics.add.sprite(playerSpawn.x, playerSpawn.y, 'Critter', 4).setScale(0.5).setOrigin(0.5,0.625);
+        this.critter.setCollideWorldBounds(true);
+        this.critter.setSize(48,48);
+        this.critter.setOffset(0,16);
+        this.targetX = this.critter.x;
+        this.dirX;
+        this.dirY;
+        this.targetY = this.critter.y;
+
+        //createPlayerAnimations.call(this);
         this.keys = this.scene.input.keyboard.addKeys(
             {
                 left: Phaser.Input.Keyboard.KeyCodes.LEFT,
@@ -38,5 +52,64 @@ class Critter extends Animals {
         );
         this.damageMax = 3;
     }
-    
+    update(){
+      this.checkPlayerMovement();
+    }
+    checkPlayerMovement(){
+      console.log(this.critter.x + ", " + this.targetX);
+      if (this.keys.right.isDown && this.critter.x == this.targetX) {
+        this.dirX = 1;
+          this.critter.body.velocity.x = 100;
+          this.targetX = this.critter.x + 24;
+          this.critter.anims.play('walkSide', true);
+          this.critter.flipX = false;
+        } else if (this.critter.x > this.targetX && this.dirX == 1) {
+          this.critter.body.velocity.x = 0;
+          this.critter.x = this.targetX;
+        } else if (this.dirX == 1 && this.keys.left.isDown && ((this.critter.x-12)%24)==0) {
+          this.targetX = this.critter.x;
+        }
+
+        if (this.keys.left.isDown && this.critter.x == this.targetX) {
+          this.dirX = 0;
+            this.critter.body.velocity.x = -100;
+            this.targetX = this.critter.x - 24;
+            this.critter.anims.play('walkSide', true);
+            this.critter.flipX = true;
+          } else if (this.critter.x < this.targetX && this.dirX == 0) {
+            this.critter.body.velocity.x = 0;
+            this.critter.x = this.targetX;
+          }else if (this.dirX == 0 && this.keys.right.isDown && ((this.critter.x-12)%24)==0) {
+
+            this.targetX = this.critter.x;
+          }
+
+//UP DOWN
+          if (this.keys.down.isDown && this.critter.y == this.targetY) {
+            this.dirY = 1;
+              this.critter.body.velocity.y = 100;
+              this.targetY = this.critter.y + 24;
+              this.critter.anims.play('walkDown', true);
+            } else if (this.critter.y > this.targetY && this.dirY == 1) {
+              this.critter.body.velocity.y = 0;
+              this.critter.y = this.targetY;
+            } else if (this.dirY == 1 && this.keys.up.isDown && ((this.critter.y-12)%24)==0) {
+              this.targetY = this.critter.y;
+            }
+
+            if (this.keys.up.isDown && this.critter.y == this.targetY) {
+              this.dirY = 0;
+                this.critter.body.velocity.y = -100;
+                this.targetY = this.critter.y - 24;
+                this.critter.anims.play('walkUp', true);
+              } else if (this.critter.y < this.targetY && this.dirY == 0) {
+                this.critter.body.velocity.y = 0;
+                this.critter.y = this.targetY;
+              }else if (this.dirY == 0 && this.keys.down.isDown && ((this.critter.y-12)%24)==0) {
+
+                this.targetY = this.critter.y;
+              }
+
+    }
+
 }
